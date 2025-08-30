6fa6c6b6a523bbc2c0f3c7d6cec96dd8af9d5dba
ASHISH7865 <jaiswalashish9467@gmail.com>
2025-08-30 22:30:26 +0530

    removed dist

--- backend/dist/src/routes/session.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-router.get("/:id", () => {
-    console.log("return specfic session data");
-});
-router.post("/", () => {
-    console.log("sessions");
-});
-export default router;
